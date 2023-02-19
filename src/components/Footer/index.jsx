d75e0c36b14bfb966f26c1960688c0d198fdcf30
Sergii Mykolyshyn <smgtasanandreas190@gmail.com>
2023-02-19 14:09:34 +0000

    Message to footer component
    - Added a message to footer component, letting people know how to submit their feedback or report bugs

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,6 +5,26 @@ export const Footer = ({ contractVersion, frontendNodeAddress }) => {
   return (
     <Container textAlign="center">
       <Divider hidden />
+      <p>
+        The platform is in testing stage. If you find any issues, errors or
+        unexpected behaviour while using our platform, please contact us via{' '}
+        <a
+          href="https://discord.gg/7WEbtmuqtv"
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+        >
+          Discord
+        </a>{' '}
+        or{' '}
+        <a
+          href="https://github.com/optriment/optrispace-frontend-v2/issues/new"
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+        >
+          GitHub
+        </a>
+        . We would appreciate your help!
+      </p>
       &copy; 2023 OPTRIMENT LLC
       {contractVersion && (
         <>
